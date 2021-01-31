f4b6a660243b63c9a1450665cf827a020e4b85af
christopheryou <70591924+christopheryou@users.noreply.github.com>
2021-01-31 00:06:55 -0500

    Add files via upload

---
+++ config.h
@@ -0,0 +1,15 @@
+/** CSci-4611 Assignment 6: Harold
+ */
+
+
+// The file config.h.in is processed by cmake to produce config.h.  This
+// replaces strings of the form "at"CMAKE_VARIABLE_NAME"at" with the value
+// of the corresponding cmake variable, allowing us to pass directory paths
+// and other information configured with cmake into our C++ code.
+
+
+#define DATA_DIR_BUILD "C:/Users/chris/repo-you00017/dev/a6-harold/data"
+#define DATA_DIR_INSTALL "C:/Users/chris/repo-you00017//share/a6-harold/data"
+
+#define SHADERS_DIR_BUILD "C:/Users/chris/repo-you00017/dev/a6-harold/shaders"
+#define SHADERS_DIR_INSTALL "C:/Users/chris/repo-you00017//share/a6-harold/shaders"
